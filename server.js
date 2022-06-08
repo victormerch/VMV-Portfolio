b11fc17a7a864585fd19b63df0353616b3d20a8d
victormerch <1Victormerch@gmail.com>
2022-06-08 13:57:47 +0200

    fdd

--- server.js
+++ server.js
@@ -42,5 +42,6 @@ app.get('/sendEmail', (req, res) => {
   });
 })
 
-var port_number = server.listen(process.env.PORT || 3000);
-app.listen(port_number);
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
