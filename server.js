a6a71db6712a989bac0ecd5447fa6a60f29aad46
victormerch <1Victormerch@gmail.com>
2022-06-08 17:58:12 +0200

    fddsssds

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express')
 const path = require('path');
 var nodemailer = require('nodemailer');
 const app = express()
-const port = 9000
+const PORT  = process.env.PORT || 5000
 app.use(express.static(__dirname));
 
 app.get('/', (req, res) => {
