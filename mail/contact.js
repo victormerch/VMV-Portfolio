c26a66d435d127c7c7f708fd433ab05613cbef66
victormerch <1Victormerch@gmail.com>
2022-06-08 13:42:43 +0200

    protafolio 1.0

--- mail/contact.js
+++ mail/contact.js
@@ -30,7 +30,7 @@ $(function () {
                         $('#success > .alert-success').html("<button type='button' class='close' data-dismiss='alert' aria-hidden='true'>&times;")
                                 .append("</button>");
                         $('#success > .alert-success')
-                                .append("<strong>Your message has been sent. </strong>");
+                                .append("<strong>Mesnsaje enviado!. </strong>");
                         $('#success > .alert-success')
                                 .append('</div>');
                         $('#contactForm').trigger("reset");
@@ -38,7 +38,7 @@ $(function () {
                         $('#success').html("<div class='alert alert-danger'>");
                         $('#success > .alert-danger').html("<button type='button' class='close' data-dismiss='alert' aria-hidden='true'>&times;")
                                 .append("</button>");
-                        $('#success > .alert-danger').append($("<strong>").text("Sorry " + name + ", it seems that our mail server is not responding. Please try again later!"));
+                        $('#success > .alert-danger').append($("<strong>").text("Lo siento " + name + ", no se pudo enviar el email. Porfavor prueba otra vez!"));
                         $('#success > .alert-danger').append('</div>');
                         $('#contactForm').trigger("reset");
                     }
@@ -48,7 +48,7 @@ $(function () {
                     $('#success').html("<div class='alert alert-danger'>");
                     $('#success > .alert-danger').html("<button type='button' class='close' data-dismiss='alert' aria-hidden='true'>&times;")
                             .append("</button>");
-                    $('#success > .alert-danger').append($("<strong>").text("Sorry " + name + ", it seems that our mail server is not responding. Please try again later!"));
+                    $('#success > .alert-danger').append($("<strong>").text("Lo siento " + name + ", no se pudo enviar el email. Porfavor prueba otra vez!"));
                     $('#success > .alert-danger').append('</div>');
                     $('#contactForm').trigger("reset");
                 },
